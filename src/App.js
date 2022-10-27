19aa4331704ae7faf75197dc2d87c37991e64717
Connor <connor5741213@gmail.com>
2022-10-26 21:23:25 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SeachBar'
+import { DataContext } from './context/DataContext'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    let [searchTerm, setSearchTerm] = useState('')
+    let [data, setData] = useState([])
+    let [message, setMessage] = useState('Search for Music!')
+    
+
+    {/* ... */}
+
+    return (
+        <div className="App">
+            <SearchBar handleSearch={handleSearch} />
+            {message}
+            <DataContext.Provider value={data} >
+                <Gallery />
+            </DataContext.Provider>
+        </div>
+    )
 }
 
-export default App;
