e0bdcf2662c2c44c9bc11b03b8a1c5407bfac555
Connor <connor5741213@gmail.com>
2022-10-28 18:59:26 -0400

    up to part 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
-import SearchBar from './components/SeachBar'
+import SearchBar from './components/Searchbar'
 import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 
 const App = () => {
-    let [searchTerm, setSearchTerm] = useState('')
     let [data, setData] = useState([])
     let [message, setMessage] = useState('Search for Music!')
+    let searchInput = useRef('')
     
 
-    {/* ... */}
+    const API_URL = 'https://itunes.apple.com/search?term='
+
+	const handleSearch = (e, term) => {
+		e.preventDefault()
+		// Fetch Data
+		const fetchData = async () => {
+			document.title = `${term} Music`
+			const response = await fetch(API_URL + term)
+			const resData = await response.json()
+			if (resData.results.length > 0) {
+                // Set State and Context value
+				return setData(resData.results)
+			} else {
+				return setMessage('Not Found')
+			}
+		}
+		fetchData()
+	}
 
     return (
         <div className="App">
-            <SearchBar handleSearch={handleSearch} />
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar />
+                </SearchContext.Provider>
             {message}
             <DataContext.Provider value={data} >
                 <Gallery />
             </DataContext.Provider>
         </div>
-    )
+    );
 }
 
+export default App;
